670940e921901a470e6cb6b20875ff0ec04e8b42
arifinavicena <avicenaarifin@gmail.com>
2020-06-16 21:37:44 +0700

    hide pricing

--- src/components/header.component.js
+++ src/components/header.component.js
@@ -17,7 +17,7 @@ export default class Header extends Component {
                     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
                         <Link to="/" className="mr-5 hover:text-gray-900 cursor-pointe">Home</Link>
                         <Link to="/recipes" className="mr-5 hover:text-gray-900 cursor-pointe">Recipes</Link>
-                        <Link to="/pricing" className="mr-5 hover:text-gray-900 cursor-pointe">Pricing</Link>
+                        {/* <Link to="/pricing" className="mr-5 hover:text-gray-900 cursor-pointe">Pricing</Link> */}
                         <Link to="/about" className="mr-5 hover:text-gray-900 cursor-pointe">About Us</Link>
                     </nav>
                 </div>
