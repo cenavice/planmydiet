4d709a68d5c2ebcfd40e0af443e7a3e8f336a414
arifinavicena <avicenaarifin@gmail.com>
2020-06-16 20:47:18 +0700

    use link on header

--- src/components/header.component.js
+++ src/components/header.component.js
@@ -6,19 +6,19 @@ export default class Header extends Component {
         return (
             <header className="text-gray-700 body-font">
                 <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-                    <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 cursor-pointer">
+                    <Link to="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 cursor-pointer">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round"
                             stroke-linejoin="round" stroke-width="2" className="w-10 h-10 text-white p-2 bg-green-500 rounded-full"
                             viewBox="0 0 24 24">
                             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
                         </svg>
                         <span className="ml-3 text-xl">PlanMyDiet</span>
-                    </a>
+                    </Link>
                     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                        <a href="index.html" className="mr-5 hover:text-gray-900 cursor-pointer">Home</a>
-                        <a href="recipe.html" className="mr-5 hover:text-gray-900 cursor-pointer">Recipe</a>
-                        <a href="pricing.html" className="mr-5 hover:text-gray-900 cursor-pointer">Pricing</a>
-                        <a href="about.html" className="mr-5 hover:text-gray-900 cursor-pointer">About Us</a>
+                        <Link to="/" className="mr-5 hover:text-gray-900 cursor-pointe">Home</Link>
+                        <Link to="/recipes" className="mr-5 hover:text-gray-900 cursor-pointe">Recipe</Link>
+                        <Link to="/pricing" className="mr-5 hover:text-gray-900 cursor-pointe">Pricing</Link>
+                        <Link to="/about" className="mr-5 hover:text-gray-900 cursor-pointe">About Us</Link>
                     </nav>
                 </div>
             </header>
