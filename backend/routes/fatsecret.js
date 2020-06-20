996cc46dc9e59b40f318e9f4fb67548c04a9240c
arifinavicena <avicenaarifin@gmail.com>
2020-06-20 21:19:42 +0700

    get data from fatsecret

---
+++ backend/routes/fatsecret.js
@@ -0,0 +1,75 @@
+const router = require('express').Router();
+const axios = require('axios');
+const querystring = require('querystring');
+const flatted = require('flatted');
+
+let token = '';
+let expiresIn;
+
+router.route('/get-token').get(async (req, res) => {
+    const clientID = process.env.FATSECRET_CLIENT_ID;
+    const clientSecret = process.env.FATSECRET_CLIENT_SECRET;
+    
+    let formData = {
+        'grant_type': 'client_credentials',
+        'scope': 'basic'
+    }
+    formData = querystring.stringify(formData);
+
+    const putToken = (result) => { 
+        token = result.data.access_token;
+        expiresIn = result.data.expires_in;
+    };
+
+    axios.post('https://oauth.fatsecret.com/connect/token', formData, {
+            headers: { 'content-type': 'application/x-www-form-urlencoded' },
+            auth: {
+                username: clientID,
+                password: clientSecret
+            },
+        })
+        .then(response => {
+            putToken(response);
+            req.session.fatSecretToken = token;
+            req.session.fatSecretTokenExpiresIn = expiresIn;
+
+            res.json(response.data);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(400).json({ access_token: '' });
+        });
+
+});
+
+router.route('/get-food').get((req, res) => {
+
+    let sessionData = {
+        token: req.session.fatSecretToken,
+        expires_in: req.session.fatSecretTokenExpiresIn
+    }
+
+    const url = 'https://platform.fatsecret.com/rest/server.api';
+
+    const headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + sessionData.token 
+    };
+
+    let formData = {
+        'method': 'food.get.v2',
+        'food_id': '1642',
+        'format': 'json'
+    }
+    
+    axios.get(url, { params: formData, headers: headers})
+        .then(response => {
+            res.send(flatted.parse(flatted.stringify(response.data)));
+        })
+        .catch(err => {
+            res.send(flatted.parse(flatted.stringify(err)));
+        });
+
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const session = require('express-session');
 
 require('dotenv').config();
 
@@ -11,4 +12,15 @@ app.use(express.json());
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
+
+app.use(session({
+    secret: 'tesa',
+    resave: true,
+    saveUninitialized: true
+}));
+
+const fatsecretRouter = require('./routes/fatsecret');
+
+app.use('/fatsecret', fatsecretRouter);
+
