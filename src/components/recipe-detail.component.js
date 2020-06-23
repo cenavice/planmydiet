1bf52f4cdf9e6f0c46202356b140040e42fa95bd
arifinavicena <avicenaarifin@gmail.com>
2020-06-23 22:46:36 +0700

    front: recipe add nutrition and percentage

--- src/components/recipe-detail.component.js
+++ src/components/recipe-detail.component.js
@@ -3,31 +3,32 @@ const axios = require("axios");
 
 function RecipeData(props) {
   const recipeData = props.data;
-  
+
   if (Object.keys(recipeData).length === 0) {
     return <h2>loading...</h2>;
   }
 
-  return <div className="flex flex-col text-left w-full mt-8 mb-8">
-    <h2 className="text-xs text-green-500 tracking-widest font-medium title-font mb-1">
-      {props.data.recipeType}
-    </h2>
-    <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
-      {props.data.recipeName}
-    </h1>
-    <p className="leading-relaxed text-base">
-    {props.data.recipeDescription}
-    </p>
-    <ul className="flex flex-wrap list-none mt-3">
-      <li className="lg:w-1/2 mb-1 w-1/2 text-left">
-        <p className="text-gray-600">
-          Yields :{" "}
-          <span className="text-green-700 font-semibold">
-            {props.data.numberOfServings} Servings
-          </span>
-        </p>
-      </li>
-      {/* <li className="lg:w-1/2 mb-1 w-1/2 text-right">
+  return (
+    <div className="flex flex-col text-left w-full mt-8 mb-8">
+      <h2 className="text-xs text-green-500 tracking-widest font-medium title-font mb-1">
+        {props.data.recipeType}
+      </h2>
+      <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+        {props.data.recipeName}
+      </h1>
+      <p className="leading-relaxed text-base">
+        {props.data.recipeDescription}
+      </p>
+      <ul className="flex flex-wrap list-none mt-3">
+        <li className="lg:w-1/2 mb-1 w-1/2 text-left">
+          <p className="text-gray-600">
+            Yields :{" "}
+            <span className="text-green-700 font-semibold">
+              {props.data.numberOfServings} Servings
+            </span>
+          </p>
+        </li>
+        {/* <li className="lg:w-1/2 mb-1 w-1/2 text-right">
         <p className="text-gray-600">
           Total Time :{" "}
           <span className="text-green-700 font-semibold">
@@ -35,8 +36,9 @@ function RecipeData(props) {
           </span>
         </p>
       </li> */}
-    </ul>
-  </div>;
+      </ul>
+    </div>
+  );
 }
 
 function IngredientList(props) {
@@ -53,6 +55,175 @@ function IngredientList(props) {
   }
 
   return <ul>{listItems}</ul>;
+}
+
+function NutritionList(props) {
+  const nutiritions = props.nutiritions;
+
+  console.log(nutiritions);
+
+  if (Object.keys(nutiritions).length === 0) {
+    return <h2>loading...</h2>;
+  }
+
+  function calculatePercentage(nutritionType, value) {
+    if (nutritionType === 'fat') {
+      return Math.ceil(value / 65 * 100);
+    }
+    
+    if (nutritionType === 'saturated_fat') {
+      return Math.ceil(value / 20 * 100);
+    }
+    
+    if (nutritionType === 'cholesterol') {
+      return Math.ceil(value / 300 * 100);
+    }
+    
+    if (nutritionType === 'sodium') {
+      return Math.ceil(value / 2400 * 100);
+    }
+    
+    if (nutritionType === 'carbohydrate') {
+      return Math.ceil(value / 300 * 100);
+    }
+    
+    if (nutritionType === 'fiber') {
+      return Math.ceil(value / 25 * 100);
+    }
+
+    if (nutritionType === 'protein') {
+      return Math.ceil(value / 50 * 100);
+    }
+
+    if (nutritionType === 'vitamin_d') {
+      return Math.ceil(value / 50 * 100);
+    }
+
+    if (nutritionType === 'calcium') {
+      return Math.ceil(value / 50 * 100);
+    }
+
+    if (nutritionType === 'iron') {
+      return Math.ceil(value / 50 * 100);
+    }
+
+    if (nutritionType === 'potassium') {
+      return Math.ceil(value / 50 * 100);
+    }
+
+    if (nutritionType === 'vitamin_a') {
+      return Math.ceil(value / 50 * 100);
+    }
+
+    if (nutritionType === 'vitamin_c') {
+      return Math.ceil(value / 50 * 100);
+    }
+  }
+
+  return (
+    <div>
+      <h3 className="text-xs">Amount Per Serving</h3>
+      <h4 className="">
+        Calories
+        <span className="float-right font-bold">{nutiritions.calories}</span>
+      </h4>
+      <hr className="mb-1 mt-1" />
+      <ul>
+        <li className="text-sm text-right font-semibold">% Daily Value*</li>
+        <li className="text-sm font-semibold">
+          Total Fat <span className="text-green-700">{nutiritions.fat}g</span>
+          <span className="float-right font-semibold">{calculatePercentage('fat', nutiritions.fat)}%</span>
+        </li>
+        <li className="text-xs ml-3">
+          Saturated Fat{" "}
+          <span className="text-green-700">{nutiritions.saturated_fat}g</span>
+          <span className="float-right font-semibold">{calculatePercentage('saturated_fat', nutiritions.saturated_fat)}%</span>
+        </li>
+        <li className="text-xs ml-3">
+          Trans Fat{" "}
+          <span className="text-green-700">{nutiritions.trans_fat}g</span>
+          <span className="float-right font-semibold"></span>
+        </li>
+        <li className="text-xs ml-3">
+          Polyunsaturated Fat{" "}
+          <span className="text-green-700">
+            {nutiritions.polyunsaturated_fat}g
+          </span>
+          <span className="float-right font-semibold"></span>
+        </li>
+        <li className="text-xs ml-3">
+          Monounsaturated Fat{" "}
+          <span className="text-green-700">
+            {nutiritions.monounsaturated_fat}g
+          </span>
+          <span className="float-right font-semibold"></span>
+        </li>
+        <li className="text-sm font-semibold">
+          Cholesterol{" "}
+          <span className="text-green-700">{nutiritions.cholesterol}mg</span>
+          <span className="float-right font-semibold">{calculatePercentage('cholesterol', nutiritions.cholesterol)}%</span>
+        </li>
+        <li className="text-sm font-semibold">
+          Sodium <span className="text-green-700">{nutiritions.sodium}mg</span>
+          <span className="float-right font-semibold">{calculatePercentage('sodium', nutiritions.sodium)}%</span>
+        </li>
+        <li className="text-sm font-semibold">
+          Total Carbohydrate{" "}
+          <span className="text-green-700">{nutiritions.carbohydrate}g</span>
+          <span className="float-right font-semibold">{calculatePercentage('carbohydrate', nutiritions.carbohydrate)}%</span>
+        </li>
+        <li className="text-xs ml-3">
+          Dietary Fiber{" "}
+          <span className="text-green-700">{nutiritions.fiber}g</span>
+          <span className="float-right font-semibold">{calculatePercentage('fiber', nutiritions.fiber)}%</span>
+        </li>
+        <li className="text-xs ml-3">
+          Sugars <span className="text-green-700">{nutiritions.sugar}g</span>
+          <span className="float-right font-semibold"></span>
+        </li>
+        <li className="text-sm font-semibold">
+          Protein <span className="text-green-700">{nutiritions.protein}g</span>
+          <span className="float-right font-semibold">6%</span>
+        </li>
+        <hr className="mb-1 mt-1" />
+        <li className="text-xs">
+          Vitamin D{" "}
+          <span className="text-green-700">{nutiritions.vitamin_d ?? "-"}</span>
+          <span className="float-right"></span>
+        </li>
+        <li className="text-xs">
+          Calcium{" "}
+          <span className="text-green-700">{nutiritions.calcium}mg</span>
+          <span className="float-right">4%</span>
+        </li>
+        <li className="text-xs">
+          Iron <span className="text-green-700">{nutiritions.iron}mg</span>
+          <span className="float-right">15%</span>
+        </li>
+        <li className="text-xs">
+          Potassium{" "}
+          <span className="text-green-700">{nutiritions.potassium}mg</span>
+          <span className="float-right">17%</span>
+        </li>
+        <li className="text-xs">
+          Vitamin A{" "}
+          <span className="text-green-700">{nutiritions.vitamin_a}mcg</span>
+          <span className="float-right">2%</span>
+        </li>
+        <li className="text-xs">
+          Vitamin C{" "}
+          <span className="text-green-700">{nutiritions.vitamin_c}mg</span>
+          <span className="float-right">14%</span>
+        </li>
+      </ul>
+      <hr className="mb-1 mt-1" />
+      <p className="text-xs">
+        * The % Daily Value (DV) tells you how much a nutrient in a serving of
+        food contributes to a daily diet. 2,000 calories a day is used for
+        general nutrition advice.
+      </p>
+    </div>
+  );
 }
 
 function DirectionList(props) {
@@ -86,8 +257,8 @@ export default class RecipeDetail extends Component {
       recipeId: "",
       recipeType: "",
       servingSizes: [],
-      recipeData: {}
-    }
+      recipeData: {},
+    };
   }
 
   componentDidMount() {
@@ -102,15 +273,17 @@ export default class RecipeDetail extends Component {
           recipeName: response.data.recipe.recipe_name,
           recipeId: response.data.recipe.recipe_id,
           recipeType: response.data.recipe.recipe_types.recipe_type,
-          servingSizes: response.data.recipe.serving_sizes.servings,
+          servingSizes: response.data.recipe.serving_sizes.serving,
           recipeData: {
             recipeDescription: response.data.recipe.recipe_description,
             recipeName: response.data.recipe.recipe_name,
             recipeId: response.data.recipe.recipe_id,
             recipeType: response.data.recipe.recipe_types.recipe_type,
             numberOfServings: response.data.recipe.number_of_servings,
-          }
-        })
+          },
+        });
+
+        console.log(response.data.recipe.serving_sizes.calories);
       })
       .catch(function (error) {
         console.log(error);
@@ -129,7 +302,7 @@ export default class RecipeDetail extends Component {
                 src="https://dummyimage.com/1200x500"
               />
             </div>
-            
+
             <RecipeData data={this.state.recipeData} />
             <div className="flex flex-col sm:flex-row mt-10">
               <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
@@ -143,95 +316,7 @@ export default class RecipeDetail extends Component {
                   <h2 className="text-gray-900 text-xl title-font font-medium mb-1">
                     Nutrition Facts
                   </h2>
-                  <h3 className="text-xs">Amount Per Serving</h3>
-                  <h4 className="">
-                    Calories<span className="float-right font-bold">330</span>
-                  </h4>
-                  <hr className="mb-1 mt-1" />
-                  <ul>
-                    <li className="text-sm text-right font-semibold">
-                      % Daily Value*
-                    </li>
-                    <li className="text-sm font-semibold">
-                      Total Fat <span className="text-green-700">3.08g</span>
-                      <span className="float-right font-semibold">4%</span>
-                    </li>
-                    <li className="text-xs ml-3">
-                      Saturated Fat{" "}
-                      <span className="text-green-700">0.798g</span>
-                      <span className="float-right font-semibold">4%</span>
-                    </li>
-                    <li className="text-xs ml-3">
-                      Trans Fat <span className="text-green-700">0.06g</span>
-                      <span className="float-right font-semibold"></span>
-                    </li>
-                    <li className="text-xs ml-3">
-                      Polyunsaturated Fat{" "}
-                      <span className="text-green-700">0.714g</span>
-                      <span className="float-right font-semibold"></span>
-                    </li>
-                    <li className="text-xs ml-3">
-                      Monounsaturated Fat{" "}
-                      <span className="text-green-700">0.729g</span>
-                      <span className="float-right font-semibold"></span>
-                    </li>
-                    <li className="text-sm font-semibold">
-                      Cholesterol <span className="text-green-700">137mg</span>
-                      <span className="float-right font-semibold">46%</span>
-                    </li>
-                    <li className="text-sm font-semibold">
-                      Sodium <span className="text-green-700">1952mg</span>
-                      <span className="float-right font-semibold">85%</span>
-                    </li>
-                    <li className="text-sm font-semibold">
-                      Total Carbohydrate{" "}
-                      <span className="text-green-700">15.16g</span>
-                      <span className="float-right font-semibold">6%</span>
-                    </li>
-                    <li className="text-xs ml-3">
-                      Dietary Fiber <span className="text-green-700">0.6g</span>
-                      <span className="float-right font-semibold">2%</span>
-                    </li>
-                    <li className="text-xs ml-3">
-                      Sugars <span className="text-green-700">10g</span>
-                      <span className="float-right font-semibold"></span>
-                    </li>
-                    <li className="text-sm font-semibold">
-                      Protein <span className="text-green-700">56.95g</span>
-                      <span className="float-right font-semibold">6%</span>
-                    </li>
-                    <hr className="mb-1 mt-1" />
-                    <li className="text-xs">
-                      Vitamin D <span className="text-green-700"></span>
-                      <span className="float-right"></span>
-                    </li>
-                    <li className="text-xs">
-                      Calcium <span className="text-green-700">46mg</span>
-                      <span className="float-right">4%</span>
-                    </li>
-                    <li className="text-xs">
-                      Iron <span className="text-green-700">2.69mg</span>
-                      <span className="float-right">15%</span>
-                    </li>
-                    <li className="text-xs">
-                      Potassium <span className="text-green-700">803mg</span>
-                      <span className="float-right">17%</span>
-                    </li>
-                    <li className="text-xs">
-                      Vitamin A <span className="text-green-700">14mcg</span>
-                      <span className="float-right">2%</span>
-                    </li>
-                    <li className="text-xs">
-                      Vitamin C <span className="text-green-700">12.3mg</span>
-                      <span className="float-right">14%</span>
-                    </li>
-                  </ul>
-                  <hr className="mb-1 mt-1" />
-                  <p className="text-xs">
-                    * The % Daily Value (DV) tells you how much a nutrient in a
-                    serving of food contributes to a daily diet. 2,000 calories
-                    a day is used for general nutrition advice.
-                  </p>
+                  <NutritionList nutiritions={this.state.servingSizes} />
                 </div>
               </div>
               <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-300 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
