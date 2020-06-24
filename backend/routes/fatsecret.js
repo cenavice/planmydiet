3f1dafc3f0ada64c4e730e0da77b09b2e33fcd15
arifinavicena <avicenaarifin@gmail.com>
2020-06-24 20:59:30 +0700

    front: add recipe list from API

--- backend/routes/fatsecret.js
+++ backend/routes/fatsecret.js
@@ -72,10 +72,11 @@ router.route('/get-food').get((req, res) => {
 
 });
 
-router.route('/search-recipe').get((req, res) => {
+router.route('/search-recipe').post((req, res) => {
     
     let sessionData = {
-        token: req.session.fatSecretToken,
+        // token: req.session.fatSecretToken,
+        token: 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjQ1MjZBMkFCNkQ0MkQ5REIwMjBEMThBRDMxRTE5MTdCMUUzMjg2RTUiLCJ0eXAiOiJhdCtqd3QiLCJ4NXQiOiJSU2FpcTIxQzJkc0NEUml0TWVHUmV4NHlodVUifQ.eyJuYmYiOjE1OTI5MjA3NjIsImV4cCI6MTU5MzAwNzE2MiwiaXNzIjoiaHR0cHM6Ly9vYXV0aC5mYXRzZWNyZXQuY29tIiwiYXVkIjoiYmFzaWMiLCJjbGllbnRfaWQiOiI1Y2E0YWM4MzU5YzI0YTc4OTk0M2JkNzk0ODE4NDI4YSIsInNjb3BlIjpbImJhc2ljIl19.lYSX6SvTAK_aqKl0tloTvxtqnKWh1SWuCJfe9Lk71ngxpm3-bFJ41I-iiqRwBL5aQ5b2c0f3XA0rhUao-GzV6PRKKaVfvleiL-QWgzs1QOyaVcR6Gn3FcCJA0SY0BR5JQdzNNKdUH40cT0WvE9pWiOK-x5CO6pxzK-sEGGzNc8-taeT2MM2tlAb-i7Ka6hFC5jM8OvqJtK1ChJKllVl4KISrl40Lljp0np2T1N9ZFUFK_4x91-8NsshA5FxgjP5iRzGQbF44zOi6qY19Nmrgl-7lmyHgTdCa3tbmPvb9SqNM9OC8MFyjD2wOigm78yVJJZs4cHvJX5g9BnVrDilu5g',
         expires_in: req.session.fatSecretTokenExpiresIn
     }
 
@@ -87,8 +88,8 @@ router.route('/search-recipe').get((req, res) => {
     };
 
     let formData = {
-        'method': 'food.search',
-        'search_expression': 'chicken',
+        'method': 'recipes.search',
+        'search_expression': '',
         'page_number': 1,
         'max_results': 20,
         'format': 'json'
@@ -101,8 +102,6 @@ router.route('/search-recipe').get((req, res) => {
         .catch(err => {
             res.send(flatted.parse(flatted.stringify(err)));
         });
-
-    res.json('asd');
 });
 
 router.route('/get-recipe').get((req, res) => {
--- src/components/recipes-list.component.js
+++ src/components/recipes-list.component.js
@@ -1,37 +1,106 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+const axios = require("axios");
+
+function RecipeCards(props) {
+  const recipes = props.recipes;
+
+  let listItems = "loading...";
+
+  if (recipes.length !== 0) {
+    listItems = recipes.map((recipe) => (
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full" key={recipe.recipe_id}>
+        <Link
+          to={"/recipe/" + recipe.recipe_id}
+          className="block relative h-48 rounded overflow-hidden"
+        >
+          <img
+            alt="ecommerce"
+            className="object-cover object-center w-full h-full block"
+            src={recipe.recipe_image}
+          />
+        </Link>
+        <div className="mt-4">
+          {/* <h3 className="text-green-500 text-xs tracking-widest title-font mb-1">
+            CATEGORY
+          </h3> */}
+          <a
+            href="detail-recipe.html"
+            className="text-gray-900 title-font text-lg font-medium"
+          >
+            {recipe.recipe_name}
+          </a>
+          <p className="leading-relaxed text-base">
+            {recipe.recipe_description}
+          </p>
+          <p className="leading-relaxed text-xs font-medium mt-1">
+            Per serve - Energy: {recipe.recipe_nutrition.calories}kcal <br /> Carb: {recipe.recipe_nutrition.carbohydrate}g | Prot: {recipe.recipe_nutrition.protein}g |
+            Fat: {recipe.recipe_nutrition.fat}g
+          </p>
+        </div>
+      </div>
+    ));
+  }
+
+  return <div className="flex flex-wrap -m-4">{listItems}</div>;
+}
 
 export default class RecipesList extends Component {
-    render() {
-        return (
-            <section className="text-gray-700 body-font">
-                <div className="container px-5 pt-10 mx-auto">
-                    <div className="mb-12 text-center w-full">
-                        <p className="mb-3 leading-relaxed">Showing search result for : Chicken</p>
-                        <div className="flex w-3/6 mx-auto">
-                            <input className="flex-grow w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-green-500 text-base px-4 py-2 mr-4 mb-4 sm:mb-0" placeholder="Search Recipe" type="text"/>
-                            <a href="recipe.html" className="text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">Search</a>
-                        </div>
-                    </div>
-                    <div className="flex flex-wrap -m-4">
-                        <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                            <Link to={"/recipe/"+1} className="block relative h-48 rounded overflow-hidden">
-                                <img alt="ecommerce" className="object-cover object-center w-full h-full block"
-                                    src="https://dummyimage.com/420x260"/>
-                            </Link>
-                            <div className="mt-4">
-                                <h3 className="text-green-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
-                                <a href="detail-recipe.html" className="text-gray-900 title-font text-lg font-medium">The Catalyzer</a>
-                                <p className="leading-relaxed text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                                    Repellat, nesciunt! ...</p>
-                                <p className="leading-relaxed text-xs font-medium mt-1">Per serve - Energy: 261kcal <br/> Carb:
-                                    37.59g | Prot: 22.01g | Fat: 4.01g</p>
-                            </div>
-                        </div>
-                        
-                    </div>
-                </div>
-            </section>
-        );
-    }
+  constructor(props) {
+    super(props);
+
+    // this.onChangeUsername = this.onChangeUsername.bind(this);
+
+    this.state = {
+      recipes: [],
+      maxResults: "",
+      pageNumber: "",
+      totalResults: "",
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .post("http://localhost:5000/fatsecret/search-recipe/")
+      .then((response) => {
+        this.setState({
+          recipes: response.data.recipes.recipe,
+          maxResults: response.data.recipes.max_results,
+          pageNumber: response.data.recipes.page_number,
+          totalResults: response.data.recipes.total_results,
+        });
+
+        console.log(this.state.recipes);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  render() {
+    return (
+      <section className="text-gray-700 body-font">
+        <div className="container px-5 pt-10 mx-auto">
+          <div className="mb-12 text-center w-full">
+            <p className="mb-3 leading-relaxed">
+              Showing search result for : Chicken
+            </p>
+            <div className="flex w-3/6 mx-auto">
+              <input
+                className="flex-grow w-full bg-gray-100 rounded border border-gray-400 focus:outline-none focus:border-green-500 text-base px-4 py-2 mr-4 mb-4 sm:mb-0"
+                placeholder="Search Recipe"
+                type="text"
+              />
+              <a
+                href="recipe.html"
+                className="text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg"
+              >
+                Search
+              </a>
+            </div>
+          </div>
+          <RecipeCards recipes={this.state.recipes} />
+        </div>
+      </section>
+    );
+  }
 }
