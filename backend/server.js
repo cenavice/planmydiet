9ae4eb0729a2b7dd25c6a0de964330aa26196fe2
arifinavicena <avicenaarifin@gmail.com>
2020-06-27 17:20:23 +0700

    back: run cron get token every 23 hours

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+const cron = require("node-cron");
+const axios = require("axios");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,10 +12,20 @@ app.use(cors());
 app.use(express.json());
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
 
-const fatsecretRouter = require('./routes/fatsecret');
+cron.schedule("0 */23 * * *", () => {
+  console.log("running a task every minute");
+});
 
-app.use('/fatsecret', fatsecretRouter);
+const fatsecretRouter = require("./routes/fatsecret");
 
+app.use("/fatsecret", fatsecretRouter);
+
+axios
+  .get("http://localhost:5000/fatsecret/get-token/")
+  .then(() => console.log('get token success'))
+  .catch(function (error) {
+    console.log(error);
+  });
